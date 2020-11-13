4f43662063ae915197db7f75c7fdf1518a677a60
Spawn <amarthkr66@gmail.com>
2020-11-13 16:04:23 +0530

    Code Refactor

--- app/src/main/java/com/spawn/ai/di/modules/apputils/AppUtilModule.java
+++ app/src/main/java/com/spawn/ai/di/modules/apputils/AppUtilModule.java
@@ -1,10 +1,23 @@
 package com.spawn.ai.di.modules.apputils;
 
+import com.spawn.ai.utils.task_utils.AppUtils;
+
+import javax.inject.Named;
+import javax.inject.Singleton;
+
 import dagger.Module;
+import dagger.Provides;
 import dagger.hilt.InstallIn;
 import dagger.hilt.android.components.ApplicationComponent;
 
 @Module
 @InstallIn(ApplicationComponent.class)
 public class AppUtilModule {
+
+    @Provides
+    @Singleton
+    @Named("ForAppUtils")
+    public AppUtils provideAppUtils() {
+        return new AppUtils();
+    }
 }
